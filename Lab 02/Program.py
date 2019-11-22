2afdea7ec05a0ba87b1ce5fecde43448681a3676
Jturner-code <54911366+Jturner-code@users.noreply.github.com>
2019-11-21 20:56:05 -0600

    The data points completed program worked out procedures written down

--- Lab 02/Program.py
+++ Lab 02/Program.py
@@ -6,28 +6,21 @@ import pandas
 import matplotlib.pyplot as plt
 import seaborn as sns # visualization
 import numpy as np
-#You can add that header equals none s it assumes theere is no header and assigns a number as a header.
-data=pandas.read_csv("iris.data",header=None)
+data=pandas.read_csv("Missile.data",header=None)
 print(data)
-#Yes because although some overlap we can distinguish flowers if we use every diffrent part of the flower.
-#iris setosa.
-#iris versicolor and iris virginica and because they overlap.
-sns.pairplot(data=data,vars=(0,1,2,3), hue=4)
+
+sns.pairplot(data=data,vars=(0,1,2), hue=3)
 plt.show()
 data=np.array(data)
-X=data[:,0:4] #This gets all the rows and only the first 4 columns.
-Y=data[:,4]
+X=data[:,0:3] #This gets all the rows and only the first 4 columns.
+Y=data[:,3]
 print(X.shape) #(150,4)
 print(Y.shape) #(150,)
-#I printed x and y to see how it was shuffled after the random state
-#Random_state is how things are randomized and it can be assigned an int, istance, or none and behaves diffrently by what it is assigned.
-#It randomizes an array so the computer won't just have all of one type group together when its training.
-X,Y=shuffle(X,Y,random_state=0)
-trainX=X[:100]
-trainY=Y[:100]
-testX=X[100:]
-testY=Y[100:]
-clf = MLPClassifier(hidden_layer_sizes=[3,3], random_state=1, max_iter = 2000)
+trainX=X[:65]
+trainY=Y[:65]
+testX=X[65:]
+testY=Y[65:]
+clf = MLPClassifier(hidden_layer_sizes=[2], random_state=1, max_iter = 1000)
 clf.fit(trainX, trainY)
 print(clf.coefs_)
 prediction=clf.predict(testX)
